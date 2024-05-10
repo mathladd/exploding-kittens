fa479488f126366d7a650f5d1e0ab2c90c7b76d4
duynh6 <duynh6@vng.com.vn>
2024-05-10 10:04:46 +0700

    feat: add login stubs

--- exploding-kitten-client/src/hooks/useAuth.ts
+++ exploding-kitten-client/src/hooks/useAuth.ts
@@ -1,23 +1,7 @@
-import { useQuery } from '@tanstack/react-query';
-import { getMe } from 'apis/users';
-import { ENCRYPTION_SALT } from 'constants/config';
-import { stringEncryption } from 'helpers/string';
-
 export default function useAuth() {
-  // const saltedData = getCookie('saltedData');
-  const username = 'aaa';
-  const password = 'myPassword123';
-  const encryptedPassword = stringEncryption(ENCRYPTION_SALT, password);
+  const login = ({ username, password }: { username: string; password: string }) => {
+    console.log(username, password);
+  };
 
-  const { data: user, isLoading } = useQuery({
-    queryFn: () => getMe(),
-    queryKey: ['profileBasicInfo'],
-    enabled: true,
-    retry: (failureCount, error) => {
-      const err = error as unknown as { response: { status: number } };
-      return err?.response?.status !== 401 && failureCount < 3;
-    },
-  });
-
-  return { user, isLoading };
+  return { login };
 }
--- exploding-kitten-client/src/pages/login.tsx
+++ exploding-kitten-client/src/pages/login.tsx
@@ -1,12 +1,12 @@
 import BaseLayout from 'layout/BaseLayout';
 import Stack from 'components/Stack';
-import LobbyModule from 'modules/lobbyModule';
+import LoginModule from 'modules/loginModule';
 
 export default function LobbyPage() {
   return (
     <BaseLayout>
       <Stack className="flex-col flex-1 relative w-full min-h-full justify-center items-center my-4">
-        <LobbyModule />
+        <LoginModule />
       </Stack>
     </BaseLayout>
   );
